ea87541e78e38735314b54ac8ff37fa5fe8e82f5
govindarajanv <govindarajanv@gmail.com>
2018-07-04 09:16:54 +0530

    ruby and python exercises

---
+++ python_ruby/exercise004.py
@@ -0,0 +1,4 @@
+# Get user's name as input and greet him
+
+name = input("Enter your name:")
+print ("Hi %s,Nice to meet you" % name);
---
+++ python_ruby/exercise004.rb
@@ -0,0 +1,4 @@
+# Get user's name as input and greet him
+print "Enter your name:"
+name = gets.chomp
+puts "Hi #{name}, Nice to meet you"
